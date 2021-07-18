409541145e0024421645689b809e17a7820934bb
anohene1 <anoheneisaac@gmail.co,m>
2021-07-18 19:40:12 +0000

    add image cropping and speech settings

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:google_ml_vision/google_ml_vision.dart';
@@ -23,14 +25,91 @@ class _HomeScreenState extends State<HomeScreen> {
   PickedFile? pickedImage;
   File? imageFile;
 
+
   // This builds the interface of the Home Screen
   @override
   Widget build(BuildContext context) {
+
+    // Sets speech rate
+    double speechRate = 0.5;
+    // Sets speech volume
+    double speechVolume = 1;
+    // Sets speech pitch
+    double speechPitch = 1;
+
+
     return Scaffold(
       // This shows the blue App Bar with Audify text
       appBar: AppBar(
         title: Text('Audify'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.app_settings_alt_outlined),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) {
+                    return StatefulBuilder(builder: (context, setState) {
+                        return AlertDialog(
+                          title: Text('Settings'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: [
+                                Text('Speech Rate'),
+                                Slider(
+                                    value: speechRate,
+                                    min: 0,
+                                    max: 1,
+                                    divisions: 10,
+                                    label: speechRate.toStringAsFixed(1),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        speechRate = value;
+                                      });
+                                    }),
+                                Text('Speech Volume'),
+                                Slider(
+                                    value: speechVolume,
+                                    min: 0,
+                                    max: 1,
+                                    divisions: 10,
+                                    label: speechVolume.toStringAsFixed(1),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        speechVolume = value;
+                                      });
+                                    }),
+                                Text('Speech Pitch'),
+                                Slider(
+                                    value: speechPitch,
+                                    min: 0,
+                                    max: 1,
+                                    divisions: 10,
+                                    label: speechPitch.toStringAsFixed(1),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        speechPitch = value;
+                                      });
+                                    })
+                              ],
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Done'),
+                            )
+                          ],
+                        );
+                    });
+                  });
+            },
+          )
+        ],
       ),
       body: Container(
         child: Center(
@@ -70,7 +149,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       GoogleVisionImage.fromFile(imageFile!);
                   VisionText visionText =
                       await textRecognizer.processImage(visionImage);
-                  speak(visionText.text!);
+                  speak(text: visionText.text!, volume: speechVolume, pitch: speechPitch, rate: speechRate);
                 },
               ),
               // This button makes the text to speech engine stops reading
@@ -87,19 +166,32 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  // This function handles picking an image or taking a picture
+  // This function handles picking an image or taking a picture and cropping it
   void pickImage(ImageSource source) async {
     pickedImage = await imagePicker.getImage(source: source);
+    File? croppedFile = await ImageCropper.cropImage(
+        sourcePath: pickedImage!.path,
+        androidUiSettings: AndroidUiSettings(
+            lockAspectRatio: false,
+            toolbarWidgetColor: Colors.white,
+            toolbarTitle: 'Crop Photo',
+            toolbarColor: Colors.blue,
+            statusBarColor: Colors.blue,
+            activeControlsWidgetColor: Colors.blue,
+            backgroundColor: Colors.white,
+            hideBottomControls: true));
     setState(() {
-      imageFile = File(pickedImage!.path);
+      // imageFile = File(pickedImage!.path);
+      imageFile = File(croppedFile!.path);
     });
   }
 
   // This function handles the voice, speed and speaking of the text to speech engine
-  Future speak(String text) async {
+  Future speak({required String text, required double rate, required double pitch, required double volume}) async {
     await flutterTts.setVoice({"name": "en-gb-x-gba-local", "locale": "en-GB"});
-    // await flutterTts.setPitch(0.1);
-    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.setPitch(pitch);
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
     await flutterTts.speak(text);
   }
 }
